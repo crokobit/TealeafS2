1cf9822a21fa574041523fed3eb47e3ebf5ad5af
crokobit <crokobit@gmail.com>
2014-01-13 13:58:42 +0800

    Mod4-4

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140112212819) do
+ActiveRecord::Schema.define(version: 20140113055027) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "slug"
   end
 
   create_table "comments", force: true do |t|
